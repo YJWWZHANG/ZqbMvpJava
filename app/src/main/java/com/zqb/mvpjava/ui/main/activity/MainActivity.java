2b903e7b7da1dc465c9ef0c8e2abc2099a54a35a
zhangqianbin <zhangqianbin3@foxmail.com>
2017-08-23 18:26:33 +0800

    1.0.5

--- app/src/main/java/com/zqb/mvpjava/ui/main/activity/MainActivity.java
+++ app/src/main/java/com/zqb/mvpjava/ui/main/activity/MainActivity.java
@@ -1,8 +1,11 @@
 package com.zqb.mvpjava.ui.main.activity;
 
+import android.support.annotation.NonNull;
 import android.view.View;
 
 import com.blankj.utilcode.util.ToastUtils;
+import com.vector.update_app.HttpManager;
+import com.vector.update_app.UpdateAppManager;
 import com.zhy.autolayout.AutoFrameLayout;
 import com.zqb.mvpjava.R;
 import com.zqb.mvpjava.app.App;
@@ -12,6 +15,8 @@ import com.zqb.mvpjava.model.bean.GitHubUserInfoBean;
 import com.zqb.mvpjava.presenter.main.MainPresenter;
 import com.zqb.mvpjava.ui.undefinition.fragment.UndefinitionFragment;
 import com.zqb.mvpjava.ui.undefinition.fragment.UndefinitionFragment1;
+
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -50,7 +55,6 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
     public void onViewLongClicked(View view) {
         switch (view.getId()){
             case R.id.btn_test:
-                mPresenter.getGitHubUserInfo("yjwwzhang");
                 if (isOne){
                     isOne = false;
                     getSupportFragmentManager().beginTransaction()
@@ -60,6 +64,33 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
                     getSupportFragmentManager().beginTransaction()
                             .replace(R.id.fl_content_fragment, mUndefinitionFragment1, "b").commit();
                 }
+                mPresenter.getGitHubUserInfo("yjwwzhang");
+                new UpdateAppManager
+                        .Builder()
+                        //当前Activity
+                        .setActivity(this)
+                        //更新地址
+                        .setUpdateUrl("https://raw.githubusercontent.com/WVector/AppUpdateDemo/master/json/json.txt?appKey=ab55ce55Ac4bcP408cPb8c1Aaeac179c5f6f&version=0.1.0")
+                        //实现httpManager接口的对象
+                        .setHttpManager(new HttpManager() {
+                            @Override
+                            public void asyncGet(@NonNull String url, @NonNull Map<String, String> params, @NonNull Callback callBack) {
+
+                            }
+
+                            @Override
+                            public void asyncPost(@NonNull String url, @NonNull Map<String, String> params, @NonNull Callback callBack) {
+
+                            }
+
+                            @Override
+                            public void download(@NonNull String url, @NonNull String path, @NonNull String fileName, @NonNull FileCallback callback) {
+
+                            }
+                        })
+                        .build()
+                        .update();
+
                 break;
             default:
                 break;
